e77c792849e2d0d8ecb09feb0d33db9df06c40f2
Zheng An <zhengan@titanlix.com>
2019-01-21 15:28:26 +0800

    'save'

---
+++ src/app/bounty-task/bounty-task.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bounty-task',
+  templateUrl: './bounty-task.page.html',
+  styleUrls: ['./bounty-task.page.scss'],
+})
+export class BountyTaskPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
